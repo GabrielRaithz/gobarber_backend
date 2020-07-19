75cd18da6f553a1e66a73129e8703c473b2fb9fe
Gabriel Raithz <gabrielraithz@gmail.com>
2020-07-19 00:48:39 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,13 @@ import uploadConfig from "./config/upload";
 import 'reflect-metadata';
 import AppError from "./errors/AppError";
 
+import cors from 'cors'
+
 import './database';
 
 const app = express()
+
+app.use(cors());
 
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory));
